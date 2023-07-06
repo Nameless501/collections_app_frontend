c468c7e971ce19a63092f0b0b4b6a1b9f76fadd2
Eugene Zorin <ezorin5@yandex.ru>
2023-07-06 18:53:28 +0400

    ts error fix: child element added

--- src/components/SideBarRight.tsx
+++ src/components/SideBarRight.tsx
@@ -1,8 +1,15 @@
 import { FC } from 'react';
 import SideBarWrapper from './SideBarWrapper';
+import { Box } from '@mui/material';
 
 const SideBarRight: FC = () => {
-    return <SideBarWrapper anchor="right"></SideBarWrapper>;
+    return (
+        <SideBarWrapper anchor="right">
+            <Box>
+                
+            </Box>
+        </SideBarWrapper>
+    );
 };
 
 export default SideBarRight;
