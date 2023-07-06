d8a7c25fa396bdc1a798220961570bfcae70e8bc
Eugene Zorin <ezorin5@yandex.ru>
2023-07-06 12:29:30 +0400

    deploy error fix: added serve package dependency

--- src/features/api/configs/api.config.ts
+++ src/features/api/configs/api.config.ts
@@ -2,7 +2,7 @@ import { FetchBaseQueryArgs } from '@reduxjs/toolkit/dist/query/fetchBaseQuery';
 
 export const baseQueryOptions: FetchBaseQueryArgs = {
     baseUrl: 'https://projectapi-production-0952.up.railway.app',
-    // baseUrl: 'https://localhost:3000',
+    // baseUrl: 'http://localhost:3000',
     credentials: 'include',
 };
 
