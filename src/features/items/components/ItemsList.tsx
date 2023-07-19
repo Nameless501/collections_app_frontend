6a96ab7ed8c4a01a75cc7d6da5d3177f0df176dd
Eugene Zorin <ezorin5@yandex.ru>
2023-07-20 01:18:13 +0400

    item cards list responsive overflow fix

--- src/features/items/components/ItemsList.tsx
+++ src/features/items/components/ItemsList.tsx
@@ -5,11 +5,13 @@ import { ItemsListPropsType } from '../types/common.types';
 
 const ItemsList: FC<ItemsListPropsType> = ({ items }) => {
     return (
-        <List sx={{ width: '100%', display: 'flex', flexWrap: 'wrap' }}>
+        <List 
+            sx={{ width: '100%', display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
+        >
             {items.map((data) => (
                 <ListItem
                     key={data.item.id}
-                    sx={{ flexGrow: 1, flexBasis: '50%' }}
+                    sx={{ p: 1, flexBasis: 350, flexGrow: 1 }}
                 >
                     <ItemCardSmall item={data.item} fields={data.fields} />
                 </ListItem>
