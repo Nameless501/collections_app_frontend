40c8175bd3e8c75dc8bc78987e9c802993fa8763
Eugene Zorin <ezorin5@yandex.ru>
2023-07-24 18:59:06 +0400

    text fields markdowm parser added

--- src/features/api/configs/api.config.ts
+++ src/features/api/configs/api.config.ts
@@ -1,7 +1,8 @@
 import { FetchBaseQueryArgs } from '@reduxjs/toolkit/dist/query/fetchBaseQuery';
 
 export const baseQueryOptions: FetchBaseQueryArgs = {
-    baseUrl: 'https://projectapi-production-0952.up.railway.app',
+    // baseUrl: 'https://projectapi-production-0952.up.railway.app',
+    baseUrl: 'http://localhost:3000',
     credentials: 'include',
 };
 
--- src/features/items/components/ItemField.tsx
+++ src/features/items/components/ItemField.tsx
@@ -7,6 +7,8 @@ import {
 } from '../../../configs/common.config';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import Markdown from 'marked-react';
+import DOMPurify from 'dompurify';
 
 const ItemField: FC<IFieldValue> = ({ field, value }) => {
     return (
@@ -15,13 +17,20 @@ const ItemField: FC<IFieldValue> = ({ field, value }) => {
                 display: 'flex',
                 gap: 1,
                 width: '100%',
-                alignItems: 'center',
+                alignItems:
+                    field.type === FieldTypes.text ? 'flex-stars' : 'center',
+                flexDirection:
+                    field.type === FieldTypes.text ? 'column' : 'row',
             }}
         >
-            <Typography variant="subtitle1" color="text.secondary">
+            <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                textAlign={'left'}
+            >
                 {`${field.label}:`}
             </Typography>
-            {field.type === FieldTypes.boolean ? (
+            {field.type === FieldTypes.boolean && (
                 <>
                     {value === CheckboxFieldValues.checked ? (
                         <CheckBoxIcon />
@@ -29,7 +38,12 @@ const ItemField: FC<IFieldValue> = ({ field, value }) => {
                         <CheckBoxOutlineBlankIcon />
                     )}
                 </>
-            ) : (
+            )}
+            {field.type === FieldTypes.text && (
+                <Markdown>{DOMPurify.sanitize(value)}</Markdown>
+            )}
+            {(field.type === FieldTypes.string ||
+                field.type === FieldTypes.integer) && (
                 <Typography variant="subtitle1">{value}</Typography>
             )}
         </Box>
